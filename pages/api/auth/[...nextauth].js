2619939bef342ddab1c9756b1a29bb3c8bb5af05
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-01 22:22:25 +0100

    suppression appel module et ajout des providers Google et Spotify.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,20 @@
-// Configuration de la liaison entre Spotify, Yotube et NextAuth en utilisant les credentials dans le .env.
-import { NextApiHandler } from 'next';
+// Configuration de la liaison entre Spotify, Youtube et NextAuth en utilisant les credentials dans le .env.
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from "next-auth/providers/google";
 import SpotifyProvider from "next-auth/providers/spotify";
 import prisma from '../../../lib/prisma';
+
+export default NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+          })
+      ],
+  })
