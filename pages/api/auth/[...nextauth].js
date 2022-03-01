8baabebf4b8917bfd7d1440f6e607ab5b114c4eb
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-01 22:16:50 +0100

    Ajouts dans [...nextauth] des modules dont on aura besoin pour config Next Auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,7 @@
+// Configuration de la liaison entre Spotify, Yotube et NextAuth en utilisant les credentials dans le .env.
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from "next-auth/providers/google";
+import SpotifyProvider from "next-auth/providers/spotify";
+import prisma from '../../../lib/prisma';
