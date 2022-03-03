fb2e9f4bf616752175dc74301adf0b8000c2350c
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-03 11:44:16 +0100

    Clean de _App.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,5 @@
 import '../styles/globals.css';
-/* 
-import React from 'react';
-import NavBar from './components/NavBar';
-import Content from './components/Content';
-import RightSection from './components/RightSection'; 
-*/
-/* 
-    <div className='flex flex-row'>
-        <NavBar/>
-        <Content/>
-        <RightSection/>
-    </div> 
-*/
+import React from 'react'
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
