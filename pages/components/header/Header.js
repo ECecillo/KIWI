a7ec86a7a3e0070b3725ed09673574dfa4cd1fc3
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-03 10:17:09 +0100

    Ajout du Header et Modification de Layout
    - Header contiendra les éléments tel que la navbar et les hookers pour récupérer la session utilisateur ect...
    - Erreur dans l'ajout du dernier layout, le dossier s'était dupliqué bizarement, meme contenu mais modif du nom du dossier.

---
+++ pages/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from "../NavBar";
+
+// Composant qui va nous servir à récupérer les infos de session de l'User et les passer à la Navbar.
+export default function Header() {
+  return (
+    <>
+        <NavBar />
+    </>
+  )
+}
--- pages/components/layout/Layout.tsx
+++ pages/components/layout/Layout.tsx
@@ -1,16 +1,16 @@
 import React, { ReactNode } from "react";
-import Header from "../Header/Header";
+import Header from "../header/Header";
 
 type Props = {
     children: ReactNode;
 };
-
 
 // On définit le corp de la page (Header, content, Footer), ici on aura que la navbar dans le Header.
 const Layout: React.FC<Props> = (props) => (
     <div className='flex flex-row'>
         <Header />
         {props.children}
+        {/* Si on a besoin <Footer /> */}
     </div>
 );
 
