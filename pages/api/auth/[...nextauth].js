64049568efc8b036c179889656abfdbd6a215020
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-06 18:09:44 +0100

    Modification Module API et Ajout de différentes fonctionnalité SSR et pour les tokens.
    - Dans next auth j'ai mis une fonction refreshedToken qui permettrait si on utilisait JWT de stocker les tokens coté client.
    - La partie callback en commentaire servira au cas où on décide d'utiliser JWT.
    - On a également mis une option pages avec signIn pour spécifier une page de login que l'on créera pour lui donner un peu plus de style que juste un bouton de connexion.
    - Dans le index ajout d'un rendu coté serveur qui va s'occuper de charger la Session et lui mettre les infos que l'on utilisera.
    - Enfin dans Spotify.js on a mis les éléments de configuration pour l'object apiSpotify que l'on utilisera dans toutes l'app pour faire des appels à spotify via les méthodes du module spotify-web-api-node.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,37 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from "next-auth/providers/google";
 import SpotifyProvider from "next-auth/providers/spotify";
 import prisma from '../../../lib/prisma';
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+// https://next-auth.js.org/tutorials/refresh-token-rotation
+async function refreshAccessToken(token) {
+  try {
+    console.log("refreshAccessToken Start");
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+    // Destructure l'objet de la réponse:
+    // http://michaelthelin.se/spotify-web-api-node/#refreshAccessToken
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    // Demande un nouveau token (access_token, refreshed_token ...).
+
+    return {
+      ...token, // Le nouveau token.
+      accessToken: refreshedToken.access_token,  // Récupère le nouvelle access token.
+      // Calcule l'heure et la date à laquelle le prochain token expirera que l'on multiplie par 1000 pour avois un résultat sous la forme d'heure et pas de ms.
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      // Remplace notre ancien refresh token si on en reçoit un sinon reprend l'ancien.
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    // Si on a un problème dans la demande d'un nouveau refresh token on donne l'erreur.
+    console.error(error);
+    return {
+      ...token, // On retourne le token qui a produit l'erreur.
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,5 +45,28 @@ export default NextAuth({
     })
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
+  secret: process.env.SECRET, 
+  pages: {
+    // Spécifie une page de login spéciale lorsque l'on utilisera le /api/auth/signIn .
+    signIn: '/login',
+  },
+ /* 
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // Initial sign in
+      // Pas besoin.
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: Date.now() + account.expires_in * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        }
+      }
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+    }
+  }, */
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from './components/layout/Layout';
 import Content from './components/content/Content';
 import RightSection from './components/rightSection/RightSection';
+import { getSession, GetSessionParams } from 'next-auth/react';
 
 const Home = () => {
     return (
@@ -12,3 +13,16 @@ const Home = () => {
     )
 }
 export default Home;
+
+export async function getServerSideProps(context: GetSessionParams) {
+    // On va faire du pré-rendue sur le serveur pour récup l'access token et ensuite effectuer les requêtes à spotify.
+    const session = await getSession(context); 
+  
+    return {
+      props: {
+        session,
+      }
+    }
+  
+  }
+  
