fbe16efa0d2ad084b87956689be8441645504a2a
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-07 09:53:18 +0100

    Renomage du fichier middleware et Ajout d'un bouton dans la page de Login pour se co sans compte spotify.
    - Lorsque l'on a pas de compte on est forcé à aller sur la page de Connexion via le Middleware qui gère notre accès.
    - Pour le moment le bouton connect with Youtube est en dev, on affichera juste une alerte.
    - Mettre en place un état globale que l'on va fixer lorsque l'utilisateur clique sur continuer en tant qu'invité pour éviter de bloquer l'utilisateur sur la page de co.

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,16 +1,17 @@
-import { getToken } from "next-auth/jwt";
+import { getToken } from "next-auth/jwt"; // Va chercher le contenu du JWT.
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
     
-    const token = await getToken({req, secret: process.env.SECRET});
+    const token = await getToken({req, secret: process.env.SECRET}); 
+    // Pour avoir le token il faut lui passer la clé que l'on utilise pour crypter les données.
 
     const {pathname} = req.nextUrl;
 
     // Si le token exist on autorise l'utilisateur à passer.
     if(pathname.includes('/api/auth') || token) // Si on veut se login ou on est déjà connecté.
     {
-        return NextResponse.next(); // Fais en sorte qu'il puisse aller vers la page de connexion.
+        return NextResponse.next(); // Fais en sorte qu'il puisse aller vers la page principale.
     }
 
     if(!token && pathname !== "/login") { 
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import { getProviders, signIn } from "next-auth/react"
 import logo from '../public/kiwi_full_logo.png'
-
+import { ImYoutube2 } from "react-icons/im";
 
 
 // Avant que la page se charge (que le DOM commence à mettre le HTML), on doit préparer certains éléments côté serveur.
@@ -9,7 +9,7 @@ function Login({ providers }) { // On destructure ce que l'on a retourné en des
     return (
         <div className="flex flex-col items-center bg-gradient-to-tl from-[#FFDEE9] to-[#16a084d5] min-h-screen w-full justify-center">
             <img src={logo.src} className="App-logo pb-10" alt="logo" />
-            <div className="flex flex-col items-center bg-white bg-opacity-[0.325] p-5 rounded-xl">
+            <div className="flex flex-col items-center bg-white bg-opacity-[0.325] p-5 rounded-xl space-y-3">
                 <img className="w-52 h-30 mb-7" src="https://cdn.worldvectorlogo.com/logos/spotify-1.svg" alt="Spotify logo" />
                 {Object.values(providers).map((provider) => ( // Récupère les éléments de l'object que l'on a déclaré pour next auth.
                     <div key={provider.name}>
@@ -20,6 +20,12 @@ function Login({ providers }) { // On destructure ce que l'on a retourné en des
                         </button>
                     </div>
                 ))}
+                <div className="flex flex-row items-center space-x-2 text-black p-2 rounded-full">
+                    <button onClick={() => alert("This functionnality is not available for now.")}>
+                        Continue with
+                    </button>
+                    <ImYoutube2 className="w-[4rem] h-10" />
+                </div>
             </div>
         </div>
     );
