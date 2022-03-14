24da55bc04624caf02c881a31aa92caa2bf6236f
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-03-14 10:49:57 +0100

    FIX du problème avec le middleware en checkant existence session cookie

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,10 +6,16 @@ export async function middleware(req) {
     // Pour avoir le token il faut lui passer la clé que l'on utilise pour crypter les données.
     const { pathname } = req.nextUrl;
 
-    const session = await getSession({ req });
 
-    console.log("Middleware Session : ", session);
-
+    // Lorsqu'une session est active, ce cookie doit être initialisé.
+    // Si ce n'est pas le cas (undefined), l'user ne s'est pas connecté => On le redirige.
+    // Sinon, on peut le laisser aller sur la page principale.
+    const session = req.cookies["next-auth.session-token"];
+    if(session)
+        console.log("Middleware Session : ",session);
+    else
+        console.log("Pas init");
+    
     // Si on a bien été redirigé lors de la connexion ou que 1 des index contient les tokens utilisateur alors on redirige l'utilisateur vers la page Home. 
     if (pathname.includes('/api/auth') || session)
     {
@@ -17,7 +23,7 @@ export async function middleware(req) {
     }
 
     // Si on ne s'est jamais co spotify et google seront forcément null et si la path est différent de login (essaie d'accéder à Home sans s'être connecté) on le redirige.
-    if ((!session) && (pathname !== "/login")) {
+    if (!session && (pathname !== "/login")) {
         // Redirige l'utilisateur vers la page de connexion si il ne s'est jamais connecté.
         const url = req.nextUrl.clone();
         url.pathname = "/login";
