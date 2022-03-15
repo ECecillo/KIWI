e788dd0dddd8f2cbd14c32096c42c88228f7bd91
FIETIER LORIS p1805561 <loris.fietier@etu.univ-lyon1.fr>
2022-03-15 18:29:42 +0100

    Ajout d'une grille de musique et de cover, modification provisoire hauteur tableau deroulant

--- pages/components/musicinfo/MusicInfos.js
+++ pages/components/musicinfo/MusicInfos.js
@@ -3,7 +3,7 @@ function MusicInfos(props){
     const songs = props.playlist;
     //console.log(typeof(songs));
     return(
-        <div className="scrollbar-hide overflow-y-auto overflow-x-hidden h-3/5">
+        <div className="scrollbar-hide overflow-y-auto overflow-x-hidden h-2/5 "> {/*  modif: h-3/5  ====> h-2/5  */}
             {songs.map((elmt, index) => (
             <div className="music-infos grid grid-cols-5 items-center rounded-lg font-sans text-lg select-none px-5 bg-transparent h-14 hover:text-white hover:bg-violet-900" key={`${elmt}-${index}`}>
                 <p>{elmt.id}</p>
