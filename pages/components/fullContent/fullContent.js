82c990d672cda520aca27712ae501268bb3065d2
K1000 <camille.bernot@etu.univ-lyon1.fr>
2022-03-15 17:47:17 +0100

    Le composant fullContent prend désormais un (ou plusieurs composants wrapés) en parametre afin de les afficher cote à cote avec un fond dégradé (afin de garder une cohérence graphique)

--- pages/components/fullContent/fullContent.js
+++ pages/components/fullContent/fullContent.js
@@ -1,15 +1,12 @@
-import Content from './../content/Content';
-import Rightsection from './../rightSection/RightSection';
-
-function FullContent(){
+//Prend en parametre un (ou plusieurs composants wrapés ensembles) et les affiche cote à cote avec un fond dégradé
+function FullContent(props){
+    const compo = props.compo;
+    console.log("affichage : " + compo);
     return(
         <div className='flex flex-row basis-5/6 bg-gradient-to-br from-pink-200 to-cyan-100'>
-            <Content/>
-            <Rightsection/>
+            {compo}
         </div>
     )
 }
 
 export default FullContent;
-
-//bg-gradient-to-br from-pink-200 to-cyan-100
