41c499af5bc2b926ebfc39063aa886310fec9678
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-04-28 10:19:49 +0200

    EDIT~Exécute Recherche, Affiche les résultats retourné par API.

--- pages/components/content/Content.js
+++ pages/components/content/Content.js
@@ -1,53 +1,75 @@
 import SearchBar from '../searchbar/SearchBar';
 import MediaPlayer from "../mediaPlayer/MediaPlayer";
-import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
 import useSpotify from './../../../hooks/useSpotify';
-import DisplayGrid from './../displayGrid/DisplayGrid';
+import { sessionState } from '../../../atoms/userAtom';
+import { useRecoilValue } from 'recoil';
 
 function Content() {
-    const { data: session, status } = useSession();
-    const spotifyApi = useSpotify();
+    const session = useRecoilValue(sessionState);
+    
+    const [searchResult, setSearchResult] = useState([]);
+
+    const spotifyApi = useSpotify(session);
+
     const [releases, setReleases] = useState([]);
     useEffect(() => {
-        if(spotifyApi.getAccessToken()){
-            spotifyApi.getNewReleases({limit:6}).then((data) => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getNewReleases({ limit: 6 }).then((data) => {
                 setReleases(data.body.albums.items);
             })
         }
-    }, [session, spotifyApi]);
+    }, [session, spotifyApi]); // Chaque fois que la session et le hook change.
 
     const [releasesFR, setReleasesFR] = useState([]);
     useEffect(() => {
-        if(spotifyApi.getAccessToken()){
-            spotifyApi.getNewReleases({limit:6, country:'FR'}).then((data) => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getNewReleases({ limit: 6, country: 'FR' }).then((data) => {
                 setReleasesFR(data.body.albums.items);
-            })
+            });
         }
     }, [session, spotifyApi]);
+/* 
+    useEffect(()=> {
+        
+    }, [searchResult]);
+ */
+    console.log('Resultat db ', searchResult);
 
-    console.log(releasesFR);
+    const handlePlayMusic = (event) => {
+        console.log("Hello there", event);
+    };
 
-    return(
+    return (
         <div className="content relative mx-6 pt-6 md:h-screen lg:h-full basis-full lg:basis-10/12 dark:text-dark-white">
-            <SearchBar/>
+            <SearchBar setResult={setSearchResult}/>
             <p className='font-sans text-3xl font-semibold mt-8 mb-4 pt-5 pl-10'>Nouveautés mondiales 🌍</p>
             <div className='flex flex-row'>
-                {releases.map((elmt, index) => 
-                <div className="flex flex-col h-fit items-center rounded-2xl bg-white m-2 p-2 w-32" key={`${elmt}-${index}`}>
-                    <img src={elmt.images[0].url} className="rounded-2xl aspect-square"></img>
-                    <p className="w-full overflow-hidden truncate text-center">{elmt.name}</p>
-                </div>
+                {releases.map((elmt, index) =>
+                    <div className="flex flex-col h-fit items-center rounded-2xl bg-white m-2 p-2 w-32" key={`${elmt}-${index}`}>
+                        <img src={elmt.images[0].url} className="rounded-2xl aspect-square"></img>
+                        <p className="w-full overflow-hidden truncate text-center">{elmt.name}</p>
+                    </div>
                 )}
             </div>
 
             <p className='font-sans text-3xl font-semibold mt-8 mb-4 pt-5 pl-10'>Nouveautés France 🇫🇷</p>
             <div className='flex flex-row'>
-                {releasesFR.map((elmt, index) => 
-                <div className="flex flex-col h-fit items-center rounded-2xl bg-white m-2 p-2 w-32" key={`${elmt}-${index}`}>
-                    <img src={elmt.images[0].url} className="rounded-2xl aspect-square"></img>
-                    <p className="w-full overflow-hidden truncate text-center">{elmt.name}</p>
-                </div>
+                {releasesFR.map((elmt, index) =>
+                    <div className="flex flex-col h-fit items-center rounded-2xl bg-white m-2 p-2 w-32" key={`${elmt}-${index}`}>
+                        <img src={elmt.images[0].url} className="rounded-2xl aspect-square"></img>
+                        <p className="w-full overflow-hidden truncate text-center">{elmt.name}</p>
+                    </div>
+                )}
+            </div>
+            <div className='flex flex-row'>
+                {searchResult.map((elmt, index) =>
+                    <div className="flex flex-col h-fit items-center rounded-2xl bg-black m-2 p-2 w-32" key={`${elmt}-${index}`} onClick={handlePlayMusic}>
+                        <img src={elmt.song_thumbnails} className="rounded-2xl aspect-square"></img>
+                        <p className="w-full overflow-hidden truncate text-center">{elmt.song_name}</p>
+                        <p className="w-full overflow-hidden truncate text-center">{elmt.song_view}</p>
+                        <p className="w-full overflow-hidden truncate text-center">{elmt.song_duration}</p>
+                    </div>
                 )}
             </div>
             <MediaPlayer />
--- pages/components/searchbar/SearchBar.js
+++ pages/components/searchbar/SearchBar.js
@@ -1,14 +1,39 @@
 //svg from react-icons
-import {AiOutlineSearch} from 'react-icons/ai'
+import { useRef } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 
-function SearchBar(){
-    return(
-        <div class='w-full flex justify-center'>
-            <div class="relative flex items-center w-4/6  rounded-full focus-within:shadow-lg bg-white overflow-hidden">
-                <div class="grid place-items-center h-full text-gray-300">
-                    <AiOutlineSearch className='h-10 w-10 ml-5 mr-3'/>
-                </div>
-                <input class="peer h-14 w-full outline-none text-neutral-500 pr-2 font-sans" type="search" id="search" placeholder="Rechercher un titre, un album, un artiste..." /> 
+// Encapsuler dans un Fichier.
+async function youtubeSearchMusic(music) {
+    try {
+        const response = await fetch('/api/youtube/search/' + music, {
+            method: 'GET'
+        });
+        console.log("Hello there youtubeSearchMusic" , response);
+        return response.status === 200 ? response.json() : console.log(response);
+    } catch (error) {
+        console.log("Bruh", error);
+        return false;
+    }
+}
+
+function SearchBar({setResult}) {
+    const input = useRef(null);
+
+    const handleSearch = () => {
+        youtubeSearchMusic(input.current.value).then(response => {
+            const data = response;
+            setResult(data);  
+        });
+    };
+    return (
+        <div className='w-full flex justify-center'>
+            <div className="relative flex items-center w-4/6  rounded-full focus-within:shadow-lg bg-white overflow-hidden">
+                <button onClick={handleSearch}>
+                    <div className="grid place-items-center h-full text-gray-300">
+                        <AiOutlineSearch className='h-10 w-10 ml-5 mr-3' />
+                    </div>
+                </button>
+                <input className="peer h-14 w-full outline-none text-neutral-500 pr-2 font-sans" type="search" id="search" placeholder="Rechercher un titre, un album, un artiste..." ref={input} onKeyPress={(e) => e.key === "Enter" && handleSearch()}/>
             </div>
         </div>
     )
