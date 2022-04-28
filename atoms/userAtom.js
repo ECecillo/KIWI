c79c3aa590e50242cbd8cc3454ee2e892d561ce6
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-04-28 10:05:30 +0200

    ADD~Ajout d'un nouveau global state pour stocker les infos session.

--- atoms/userAtom.js
+++ atoms/userAtom.js
@@ -1,5 +1,13 @@
 import { atom } from "recoil";
 
+/**
+ * State globale que l'on partagera sur toutes les pages et composant que l'on pré-fetch sur le serveur.
+ */
+export const sessionState = atom({
+    key: "sessionState",
+    default: {},
+});
+
 // State que l'on partage dans plusieurs pages pour savoir si l'utilisateur est en mode invité ou il est co avec spotify;.
 export const userState = atom ({
     key: "userState",
