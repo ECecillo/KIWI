9c29a124e303f1b88c9de07581dea5fae479cebc
ECecillo <enzo.cecillon@etu.univ-lyon1.fr>
2022-04-28 10:20:15 +0200

    FIX~Ajout props session pour Header

--- pages/components/layout/Layout.tsx
+++ pages/components/layout/Layout.tsx
@@ -1,14 +1,16 @@
+import { Session } from "next-auth";
 import React, { ReactNode } from "react";
 import Header from "../header/Header";
 
 type Props = {
     children: ReactNode;
+    session: Session;
 };
 
 // On définit le corp de la page (Header, content, Footer), ici on aura que la navbar dans le Header.
 const Layout: React.FC<Props> = (props) => (
     <div className='flex flex-row h-screen'>
-        <Header />
+        <Header session={props.session} />
         {/* Va caser les composants que l'on lui mettra */}
         {props.children}
         {/* Si on a besoin <Footer /> */}
